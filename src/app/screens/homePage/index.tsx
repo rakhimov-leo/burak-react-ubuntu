c5da4309e586f0448221fefded1fc20e9fd7765b
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-08-21 02:05:30 +0900

    feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,13 +7,31 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css";
 
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "@reduxjs/toolkit";
+import { setPopularDishes } from "./slice";
+import { retrievePopularDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
+//import { create } from "domain";
+
+// ** REDUX SLICE & SELECTOR  **//
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+});
+const PopularDishesRetriever = createSelector(
+  retrievePopularDishes,
+  (popularDishes) => ({popularDishes})
+);
+
 export default function Homepage() {
+  const { setPopularDishes } = actionDispatch(useDispatch());
+  const {popularDishes} = useSelector(PopularDishesRetriever);
   // Selector: Store => Data
 
   useEffect(() => {
-//Backend server data request => Data
-
-// Slice: Data => Store
+    //Backend server data request => Data
+    // Slice: Data => Store
   }, []);
 
   return (
