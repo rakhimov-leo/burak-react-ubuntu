4613b098be2a30e6e682d407c0a9e3c374adb4c4
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-08-21 01:03:19 +0900

    feat: create homePage Redux configuration

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
@@ -8,6 +8,14 @@ import Events from "./Events";
 import "../../../css/home.css";
 
 export default function Homepage() {
+  // Selector: Store => Data
+
+  useEffect(() => {
+//Backend server data request => Data
+
+// Slice: Data => Store
+  }, []);
+
   return (
     <div className={"homepage"}>
       <Statistics />
