81dc1b1543527d9da9ea199559fc707db715be45
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-08-25 01:45:25 +0900

    fix: retreive fresh menu and modify NewDishes component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,23 +5,25 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advaertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
-import "../../../css/home.css";
-
 import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes } from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
+import "../../../css/home.css";
 //import { create } from "domain";
 
 // ** REDUX SLICE & SELECTOR  **//
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+  setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
 });
 
+
+
 export default function Homepage() {
-  const { setPopularDishes } = actionDispatch(useDispatch());
+  const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch());
   // Selector: Store => Data
 
   useEffect(() => {
@@ -37,7 +39,14 @@ export default function Homepage() {
       setPopularDishes(data);
     }).catch((err) => console.log(err));
 
-    // Slice: Data => Store
+product.getProducts({
+      page: 1,
+      limit: 4,
+      order: "createdAt",
+    }).then((data) => {
+setNewDishes(data);
+    }).catch((err) => console.log(err));
+
   }, []);
 
 
